ae2792248e20ad2d64e31f45214aed42e413d31d
Gilberth Gabriel <dev.gilberth@gmail.com>
2022-03-24 01:59:28 -0300

    feat: create express integration

---
+++ src/main/adapters/adapt-express-middleware.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express';
+import { Middleware } from 'presentation/utils/ports/middleware';
+
+export async function adaptMiddleware(middleware: Middleware) {
+  return () => async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const httpResponse = await middleware.handle(req);
+      if (httpResponse.statusCode === 200) {
+        Object.assign(req, httpResponse.body);
+        return next();
+      }
+
+      res.status(httpResponse.statusCode).json(httpResponse.body);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ message: 'internal server error' });
+    }
+  };
+}
---
+++ src/main/server.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from '@prisma/client';
+import dotenv from 'dotenv';
+
+if (!process.env.PORT) dotenv.config();
+
+const prisma = new PrismaClient();
+prisma.$connect().then(async () => {
+  const app = (await import('./config/app')).default;
+  app.listen(process.env.PORT, () => console.log('server running'));
+});
