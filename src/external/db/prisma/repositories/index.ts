8b0a747e782318d87c5ae7d799b2353d76678f6b
Gilberth Gabriel <dev.gilberth@gmail.com>
2022-03-24 00:45:18 -0300

    feat: create pj account repository

---
+++ src/external/db/prisma/repositories/index.ts
@@ -0,0 +1,1 @@
+export * from './prisma-pj-account-repository';
---
+++ src/external/db/prisma/repositories/prisma-pj-account-repository.ts
@@ -0,0 +1,15 @@
+import { CreatePjAccountDto, PjAccount } from 'domain/entities';
+import { IPjAccountRepository } from 'domain/repositories';
+import { PrismaClient } from '@prisma/client';
+
+export class PrismaPjAccountRepository implements IPjAccountRepository {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  async create(data: CreatePjAccountDto): Promise<PjAccount> {
+    const account = await this.prisma.pjAccount.create({ data });
+    return {
+      ...account,
+      revenue: Number(account.revenue),
+    };
+  }
+}
